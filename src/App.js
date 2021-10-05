99f3094b94f4c5d409cf23e7daaed98cecf6360f
matheus.carneiro <dev.mtscarneiro@gmail.com>
2021-10-05 16:52:42 -0400

    some shit was done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TmdbConnection from "./TmdbConnection";
 
-const Init = () => {
+export default () => {
+
+    const [ movieList, setMovieList ] = useState([])
+
+    useEffect(() => {
+
+        const loadAll = async () => {
+            //Getting a movie list
+            let list = await TmdbConnection.getHomeList()
+            console.log(list)
+            setMovieList(list)
+        }
+
+        loadAll()
+    }, [])
+
     return (
-        <div>
-            <h1>Hello World!</h1>
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <div>
+                        {item.title}
+                    </div>
+                ))}
+            </section>
         </div>
     )
 }
-
-export default Init
